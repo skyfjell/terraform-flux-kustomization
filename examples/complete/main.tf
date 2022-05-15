df549fa0d36d53964f370088622ea928d4f5bd74
James <jamesedwardcooke@gmail.com>
2022-05-14 21:46:11 -0500

    Helm Release Refactor (#3)
    * Helm Release Refactor
    
    * terraform-docs: automated action
    
    * Helm Release Refactor
    
    * Bump flux Kust apiVersion. Unique helm release names.
    
    * Skip git secret checkov thing
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -3,24 +3,23 @@ provider "kubernetes" {
   config_context = "k3d-tftest"
 }
 
-module "flux-install" {
-  count   = 1
-  source  = "OmniTeqSource/install/flux"
-  version = "0.1.6"
+provider "helm" {
+  kubernetes {
+    config_path    = "~/.kube/config"
+    config_context = "k3d-tftest"
+  }
 }
 
-# Set to true after flux-install. GitRepository CRD must be created before the repo instances may be created.
-locals {
-  install_complete = true
+module "flux-install" {
+  source  = "OmniTeqSource/install/flux"
+  version = "0.2.0"
 }
 
 module "git-repository" {
-  count = local.install_complete ? 1 : 0
-
   source  = "OmniTeqSource/git-repository/flux"
-  version = "0.1.4"
+  version = "0.2.0"
 
-  name = "kustomization-git"
+  name = "kustomization-git-repository"
   url  = "https://github.com/OmniTeqSource/examples.git"
 
   # This will prevent a condition where the namespace cannot be removed if a CR for a CRD still exists.
@@ -28,10 +27,8 @@ module "git-repository" {
 }
 
 module "kustomization-git" {
-  count = local.install_complete ? 1 : 0
-
   source = "../../"
 
-  name = "kustomization-git"
+  name = "kustomization"
   path = "manifests"
 }
